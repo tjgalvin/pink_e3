5bc5797df62ebb0070924f14c8fd1b11613da0d0
Tim Galvin <gal16b@bracewell-i1.cm.hpc>
2019-04-09 17:16:56 +1000

    iworking percentile_images

--- percentile_images.py
+++ percentile_images.py
@@ -8,17 +8,21 @@ import matplotlib.pyplot as plt
 from itertools import product
 
 
-def plot_image(imgs: pu.image_binary, idx:int, out: str):
+def plot_image(imgs: pu.image_binary, idx:int, neurons: list, out: str, title: str=None):
     """Plot a specific image to a desired out path
     
     Arguments:
         imgs {pu.image_binary} -- reference to pink image binary
         idx {int} -- index of the item to plot
+        neurons {list} -- iterable that contains images of the neurons
         out {str} -- file path of the save file to make
+    
+    Keyword Arguments:
+        title {str} -- Figure title to add (default: {None})
     """
     no_chan = imgs.file_head[1]
 
-    fig, axes = plt.subplots(1,no_chan)
+    fig, (axes, axes2) = plt.subplots(2,no_chan)
 
     for c, ax in zip(range(no_chan), axes.flat):
         img = imgs.get_image(index=idx, channel=c)
@@ -26,11 +30,21 @@ def plot_image(imgs: pu.image_binary, idx:int, out: str):
         ax.imshow(img, cmap='bwr')
         pu.no_ticks(ax)
 
-    fig.tight_layout()
+    for c, ax in zip(range(no_chan), axes2.flat):
+        img = neurons[c]
+
+        ax.imshow(img, cmap='bwr')
+        pu.no_ticks(ax)
+
+    if title is not None:
+        fig.suptitle(title)
+
+    fig.tight_layout(rect=[0, 0.03, 1, 0.9])
     fig.savefig(out)
+    plt.close(fig)
 
 
-def perc(som: pu.som, ed: pu.heatmap, imgs:pu.image_binary, levels: int=10):
+def perc(som: pu.som, ed: pu.heatmap, imgs:pu.image_binary, base_out:str, levels: int=10):
     """Iterate over a SOM and return a set of images at different
     percentile level of similarity
     
@@ -38,11 +52,13 @@ def perc(som: pu.som, ed: pu.heatmap, imgs:pu.image_binary, levels: int=10):
         som {pu.som} -- instance of a loaded SOM
         ed {pu.heatmap} -- instance to the heatmap/similarity matrix
         imgs {pu.image_binary} -- reference to the image binary
-    
+        base_out {str} -- output folder to save items to 
+
     Keyword Arguments:
         levels {int} -- number of images at percentile levels to plot (default: {10})
     """
     som_dim = ed.file_head[1:]
+    som_chan = som.file_head[0]
 
     x = np.arange(som_dim[0])
     y = np.arange(som_dim[1])
@@ -53,16 +69,24 @@ def perc(som: pu.som, ed: pu.heatmap, imgs:pu.image_binary, levels: int=10):
 
     for coord in product(x,y):
         
-        argmask = (argmin[0] == coord[0]) & (argmin[1] == coord[1])
+        # argmask = (argmin[0] == coord[0]) & (argmin[1] == coord[1])
         argmask = np.argwhere((argmin[0] == coord[0]) & (argmin[1] == coord[1]))[:, 0]
 
         data = ed.data[argmask, coord[0], coord[1]]
 
         argsort = np.argsort(data)
-        perc_idx = argsort[ ((len(argsort) - 1) * (perc / len(perc))).astype(np.int) ]
+        perc_idx = argsort[ ((len(argsort) - 1) * (perc / (len(perc)-1))).astype(np.int) ]
 
-        for idx in perc_idx:
-            plot_image(imgs, idx, f"test{idx}.pdf")
+        print(som.file_head)
+        neurons = [som.get_neuron(y=coord[0], x=coord[1], channel=i) for i in range(som_chan)]
+
+        for p, idx in zip(perc, perc_idx):
+            midx = argmask[idx]
+            p_level = (p / len(perc)) * 100.
+
+            title = f"Neuron ({coord[0]}, {coord[1]}) - Top {p_level:.2f}% of distribution"
+
+            plot_image(imgs, midx, neurons, f"{base_out}/neuron_{coord[0]}_{coord[1]}_{p}_{idx}.png", title=title)
 
     
 
@@ -73,7 +97,7 @@ if __name__ == '__main__':
     parse.add_argument('som', help='Path to a PINK SOM file', nargs=1)
     parse.add_argument('similarity', help='Path to similarity matrix/mapping binary', nargs=1)
     parse.add_argument('images', help='Path to PINK image file that matches with SIMILARITY mapping', nargs=1)
-    parse.add_argument('levels', help='The number of percentile levels to plot', nargs=1)
+    parse.add_argument('levels', help='The number of percentile levels to plot', nargs=1, type=int)
     parse.add_argument('base_out', help='Base name for output', nargs=1)
 
     args = parse.parse_args()
@@ -81,8 +105,8 @@ if __name__ == '__main__':
     som = pu.som(args.som[0])
     ed = pu.heatmap(args.similarity[0])
     imgs = pu.image_binary(args.images[0])
-    levels = float(args.levels[0])
-    base = args.base_out[0]
+    levels = args.levels[0]
+    base_out = args.base_out[0]
 
 
-    perc(som, ed, imgs, levels=levels)
+    perc(som, ed, imgs, base_out, levels=levels)
